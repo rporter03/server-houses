9301ac8103c95b18772078d950346e8d3e781d0b
rporter03 <porterclayton2003@gmail.com>
2025-11-06 12:53:21 -0500

    asdasd

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const multer = require("multer");
+const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
-
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -30,6 +29,16 @@ let houses = [
             "attached garage"
         ],
         "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "farm-floor1.webp"
+            },
+            {
+                "name": "Basement",
+                "image": "farm-floor2.webp"
+            }
+        ]
     },
     {
         "_id":2,
@@ -42,6 +51,20 @@ let houses = [
             "covered deck"
         ],
         "main_image": "mountain-house.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "mountain-house1.webp"
+            },
+            {
+                "name": "Optional Lower Level",
+                "image": "mountain-house2.webp"
+            },
+            {
+                "name": "Main Level Slab Option",
+                "image": "mountain-house3.jpg"
+            }
+        ]
     },
     {
         "_id":3,
@@ -55,14 +78,30 @@ let houses = [
             "pool house"
         ],
         "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "lake-house1.webp"
+            },
+            {
+                "name": "Lower Level",
+                "image": "lake-house2.webp"
+            }
+        ]
     }
-];
+]
 
 app.get("/api/houses/", (req, res)=>{
-    console.log("in get request");
+    console.log("in get request")
     res.send(houses);
 });
 
+app.get("/api/houses/:id", (req, res)=>{
+    const house = houses.find((house)=>house._id === parseInt(req.params.id));
+    res.send(house);
+});
+
+
 app.listen(3001, () => {
-    console.log("server up and running");
+    console.log("Server is up and running");
 });
